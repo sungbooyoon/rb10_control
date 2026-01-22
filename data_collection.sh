c72527169fa2963689559e9608423831cb1b5644
sungbooyoon <yoonsb24@snu.ac.kr>
2026-01-22 13:23:27 +0900

    .

--- data_collection.sh
+++ data_collection.sh
@@ -14,10 +14,10 @@ ros2 launch easy_handeye2 calibrate.launch.py \
 # 1. Setup
 # setup robot and camera
 # ros2 launch rbpodo_moveit_config moveit.launch.py use_fake_hardware:=false
-ros2 launch realsense2_camera rs_launch.py 
-ros2 run tf2_ros static_transform_publisher -0.20317 0.009 -0.00405  0 0 0 1  tcp_rbpodo camera_link
+ros2 launch realsense2_camera rs_launch.py #1
+ros2 run tf2_ros static_transform_publisher -0.20317 0.009 -0.00405  0 0 0 1  tcp_rbpodo camera_link #2
 
-# apriltag
+# apriltag #3
 ros2 run apriltag_ros apriltag_node --ros-args \
     -r image_rect:=/camera/camera/color/image_rect_raw \
     -r camera_info:=/camera/camera/color/camera_info \
@@ -32,11 +32,11 @@ ros2 bag record -o apriltag_$(date +%Y%m%d_%H%M%S) -s mcap \
 # ============================================================
 # 2. Data Collection
 # topic pusblisher
-python3 /home/sungboo/rb10_control/scripts/demo_recorder_bridge.py --keyboard
+python3 /home/sungboo/rb10_control/scripts/demo_recorder_bridge.py --keyboard #4
 
 # record ros2 bag
 # mkdir -p /home/sungboo/rb10_control/dataset/raw
-cd /home/sungboo/rb10_control/dataset/raw
+cd /home/sungboo/rb10_control/dataset/raw #5
 ros2 bag record -o demo_$(date +%Y%m%d_%H%M%S) -s mcap \
   /tf \
   /tf_static \
